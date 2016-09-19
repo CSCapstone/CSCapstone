290b16000b5e58f03ccf887ad816aa783173e9b5
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-09-18 23:25:22 -0400

    Setup first app with an index view

--- CSCapstone/urls.py
+++ CSCapstone/urls.py
@@ -8,7 +8,7 @@ For more information please see:
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from CSCapstoneApp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
--- CSCapstoneApp/views.py
+++ CSCapstoneApp/views.py
@@ -3,7 +3,8 @@
 Created by Harris Christiansen on 9/18/16.
 """
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world!")
+	return render(request, 'index.html', {
+        'foo': 'bar',
+    })
