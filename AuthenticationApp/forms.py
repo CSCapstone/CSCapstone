54c75e0f6827318675d74c086ba380dca06757ef
jiraya <thenamanpat@gmail.com>
2016-10-05 09:42:51 -0400

    Ready for weekly meeting

--- AuthenticationApp/forms.py
+++ AuthenticationApp/forms.py
@@ -138,8 +138,7 @@ class AdminUserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super(AdminUserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])         
-
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
