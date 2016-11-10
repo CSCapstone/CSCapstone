2a67f66cce75c9e9c152d3a0ccd67eab883a03cd
Grey <chen1474@purdue.edu>
2016-11-09 22:55:25 -0500

    have the basic comments html setup

--- CommentsApp/views.py
+++ CommentsApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from . import models
+#from . import forms
+
 # Create your views here.
+def getComments(request):
+    return render(request, 'comments.html')
