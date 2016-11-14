21fa7df33e03353d8113045a6d795607007811b6
ddoshi26 <doshid@purdue.edu>
2016-11-14 06:53:16 -0800

    Created the comments.html file. The table is displayed when running the program

--- CommentsApp/views.py
+++ CommentsApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from . import models
+#from . import forms
+
+# Create your views here.
+def getComments(request):
+    return render(request, 'comments.html')
+
