2ce2c00a15efe8610006f5c0bef1b78e54ea255b
ddoshi26 <doshid@purdue.edu>
2016-11-14 18:59:43 -0800

    Completed step 2

--- CommentsApp/urls.py
+++ CommentsApp/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^comments$', views.getComments, name='Comments'),
+    url(r'^commentform$', views.getCommentForm, name='CommentForm'),
+    url(r'^addcomment$', views.addComment, name='AddComment'),
 ]
