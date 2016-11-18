b9e5362d5631079151da021661ea427979d4cace
victor lee <lee1961@purdue.edu>
2016-11-18 03:39:29 -0500

    nested comments

--- CSCapstone/urls.py
+++ CSCapstone/urls.py
@@ -8,13 +8,16 @@ For more information please see:
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
+
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'^', include('AuthenticationApp.urls')),
+	url(r'^auth', include('AuthenticationApp.urls')),
+	url(r'^comment/', include('CommentsApp.urls')),
 	url(r'^', include('CSCapstoneApp.urls')),
 	url(r'^', include('ProjectsApp.urls')),
     url(r'^', include('CompaniesApp.urls')),
     url(r'^', include('GroupsApp.urls')),
 	url(r'^', include('UniversitiesApp.urls')),
-    url(r'^', include('CommentsApp.urls')),
+
 ]
--- CommentsApp/urls.py
+++ CommentsApp/urls.py
@@ -1,8 +1,13 @@
 
 from django.conf.urls import url
 from . import views
+
+app_name = 'comment'
+
 urlpatterns = [
-    url(r'^comments$', views.getComments, name='Comments'),
-    url(r'^commentform$', views.getCommentForm, name='CommentForm'),
-    url(r'^addcomment$', views.addComment, name='AddComment'),
+    #url(r'^viewComment/$', views.getComments, name='Comments'),
+    url(r'^$', views.getComments, name='Comments'),
+    url(r'^getCommentForm/$', views.getCommentForm, name='CommentForm'),
+    url(r'^addComment/$', views.addComment, name='AddComment'),
+    url(r'^addSubComment/(?P<comment_id>[0-9]+)/$', views.addSubComment, name='addSubComment'),
 ]
