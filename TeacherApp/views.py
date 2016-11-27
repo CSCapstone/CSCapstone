a0302095dcc1c17b66e44a465409273c1181d9f1
ddoshi26 <doshid@purdue.edu>
2016-11-26 16:51:43 -0800

    Working on creating Teacher model

--- TeacherApp/views.py
+++ TeacherApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from . import models
+from . import forms
+
+def getTeachers(request): 
+	if request.user.is_authenticated():
+		teachers_list = models.Teacher.objects.all()
+		context = {
+			'teachers' : teachers_list
+		}
+
+		return render(request, 'teachers.html', context)
+	# else
+	return render(request 'autherror.html') 
