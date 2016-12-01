7062a614ea7cad9c3581df39a64207b0ffdad36a
Grey <chen1474@purdue.edu>
2016-12-01 09:54:34 -0500

    update project model

--- ProjectsApp/models.py
+++ ProjectsApp/models.py
@@ -3,15 +3,30 @@
 Created by Harris Christiansen on 10/02/16.
 """
 from django.db import models
+from AuthenticationApp.models import MyUser
+import json
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=10000)
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
+    programmingLanguage = models.CharField(max_length=200,default=None)
+    yearsOfExperience = models.IntegerField(default=0)
+    speciality = models.CharField(max_length=200,default=None)
+    createdBy = models.ManyToManyField(MyUser)
 
-    # TODO Task 3.5: Add field for company relationship
-    # TODO Task 3.5: Add fields for project qualifications (minimum required: programming language, years of experience, speciality)
+    def setProgrammingLanguage(self,x):
+    	self.programmingLanguage = json.dumps(x)
+
+    def getProgrammingLanguage(self):
+    	return json.loads(self.programmingLanguage)
+
+    def setSpeciality(self,x):
+    	self.speciality = json.dumps(x)
+
+    def getSpeciality(self):
+    	return json.loads(self.speciality)
 
     def __str__(self):
         return self.name
