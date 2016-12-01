f7f584171ab0672495ab249719d70695aa16e916
Grey <chen1474@purdue.edu>
2016-12-01 09:36:53 -0500

    update projects

--- ProjectsApp/views.py
+++ ProjectsApp/views.py
@@ -14,3 +14,61 @@ def getProjects(request):
 
 def getProject(request):
 	return render(request, 'project.html')
+
+def getProjectForm(request):
+	if request.user.is_engineer():
+		return render(request,'projectform.html')
+	return render(request,'autherror.html')
+
+def getProjectFormSuccess(request):
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            form = forms.GroupForm(request.POST)
+            if form.is_valid():
+                if models.Group.objects.filter(name__exact=form.cleaned_data['name']).exists():
+                    return render(request, 'groupform.html', {'error' : 'Error: That Group name already exists!'})
+                new_group = models.Group(name=form.cleaned_data['name'], description=form.cleaned_data['description'])
+                new_group.save()
+                context = {
+                    'name' : form.cleaned_data['name'],
+                }
+                return render(request, 'groupformsuccess.html', context)
+        else:
+            form = forms.GroupForm()
+        return render(request, 'groupform.html')
+    # render error page if user is not logged in
+    return render(request, 'autherror.html')
+
+def joinProject(request):
+    if request.user.is_authenticated():
+        in_name = request.GET.get('name', 'None')
+        in_group = models.Group.objects.get(name__exact=in_name)
+        is_student = request.user.is_student
+        in_group.members.add(request.user)
+        in_group.save();
+        request.user.group_set.add(in_group)
+        request.user.save()
+        context = {
+            'group' : in_group,
+            'userIsMember': True,
+            'is_student' : is_student
+        }
+        return render(request, 'group.html', context)
+    return render(request, 'autherror.html')
+    
+def unjoinProject(request):
+    if request.user.is_authenticated():
+        in_name = request.GET.get('name', 'None')
+        in_group = models.Group.objects.get(name__exact=in_name)
+        is_student = request.user.is_student
+        in_group.members.remove(request.user)
+        in_group.save();
+        request.user.group_set.remove(in_group)
+        request.user.save()
+        context = {
+            'group' : in_group,
+            'userIsMember': False,
+            'is_student' : is_student
+        }
+        return render(request, 'group.html', context)
+    return render(request, 'autherror.html')
