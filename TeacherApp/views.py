8a8c19f5e284d601493a8aef4e5e51d23537e4bd
ddoshi26 <doshid@purdue.edu>
2016-12-01 12:04:04 -0800

    Completed Teacher model. Need to set up editing

--- TeacherApp/views.py
+++ TeacherApp/views.py
@@ -21,11 +21,11 @@ def getTeacher(request):
 	if request.user.is_authenticated():
 		request_name = request.GET.get('name', 'None')
 		teacher_object = models.Teacher.objects.get(name__exact=request_name)
-		is_student = teacher_object.students.filter(email__exact=request.user.email)
+		#is_student = teacher_object.students.filter(email__exact=request.user.email)
 		
 		context = {
 			'teacher' : teacher_object,
-			'userIsStudent' : is_student,
+			#'userIsStudent' : is_student,
 		}
 
 		return render(request, 'teacher.html', context)
@@ -43,16 +43,15 @@ def getTeacherForm(request):
 def getTeacherFormSuccess(request):
 	if request.user.is_authenticated():
 		if request.method == 'POST':
-			form = forms.TeacherForm(request.POST, reuest.FILES)
+			form = forms.TeacherForm(request.POST, request.FILES)
 			if form.is_valid():
 				if models.Teacher.objects.filter(name__exact=form.cleaned_data['name']).exists():
 					return render(request, 'teacherform.html', {'error': 'Error: The teacher name entered already exists!'})
 				#else
 				new_teacher_object = models.Teacher(name=form.cleaned_data['name'],
 								photo=request.FILES['photo'],
-								email=form.cleaned_data['email'],
-								university=form.cleaned_data['university'])
-				new_teacher.save()
+								email=form.cleaned_data['email'])
+				new_teacher_object.save()
 				
 				context = {
 					'name': form.cleaned_data['name'],
