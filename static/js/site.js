99e3d2c9b01bee857785c482a8ba944c62d809f5
Grey <chen1474@purdue.edu>
2016-12-01 12:22:33 -0500

    add tinymce editor

--- static/js/site.js
+++ static/js/site.js
@@ -3,7 +3,28 @@
 	2016-09-18
 	Project: CSCapstone Marketplace
 */
-
+tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  theme: 'modern',
+  plugins: [
+    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen',
+    'insertdatetime media nonbreaking save table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+  ],
+  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+  image_advtab: true,
+  templates: [
+    { title: 'Test template 1', content: 'Test 1' },
+    { title: 'Test template 2', content: 'Test 2' }
+  ],
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+ });
 
 $(document).ready(function() { 
 	$(".sortable").tablesorter();
@@ -13,3 +34,5 @@ $(document).ready(function() {
 		dateFormat: 'yy-mm-dd',
 	});
 });
+
+
