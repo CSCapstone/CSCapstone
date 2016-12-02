a9e0338148883e9a4b0168aed075da7819d59366
ddoshi26 <doshid@purdue.edu>
2016-12-01 23:04:37 -0800

    Figured out a way to join the Teacher form with teh main appgit add AuthenticationApp/views.py git add AuthenticationApp/views.py git add AuthenticationApp/views.py git add AuthenticationApp/views.py git add AuthenticationApp/views.py git add AuthenticationApp/views.py !

--- AuthenticationApp/views.py
+++ AuthenticationApp/views.py
@@ -58,6 +58,10 @@ def auth_register(request):
 		new_user.save()	
 		login(request, new_user);	
 		messages.success(request, 'Success! Your account was created.')
+		
+		if form.cleaned_data['professor']==True:
+			return render(request, 'teacherform.html')
+
 		return render(request, 'index.html')
 
 	context = {
