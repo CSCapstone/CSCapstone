ca30c924d440dd42c982c996013e7acc7b15c666
Grey <chen1474@purdue.edu>
2016-12-03 16:42:56 -0500

    finish up with bookmarks

--- CSCapstoneApp/views.py
+++ CSCapstoneApp/views.py
@@ -6,24 +6,32 @@ Created by Harris Christiansen on 9/18/16.
 from __future__ import print_function
 from django.shortcuts import render
 from ProjectsApp.models import Project
+from ProjectsApp.models import Bookmarks
 from GroupsApp.models import Group
 from UniversitiesApp.models import University
 from CompaniesApp.models import Company
 def getIndex(request):
-	projects_list = Project.objects.all()
-	groups_list = Group.objects.all()
-	universities_list = University.objects.all()
-	companies_list = Company.objects.all()
-	context = {
-		'projects' : projects_list,
-		'groups' : groups_list,
-		'universities' : universities_list,
-		'companies' : companies_list
-	}
-	return render(request, 'index.html', context)
+    projects_list = Project.objects.all()
+    bookmarks_list = Bookmarks.objects.all()
+    groups_list = Group.objects.all()
+    universities_list = University.objects.all()
+    companies_list = Company.objects.all()
+    bookmarks = []
+    for bookmark in bookmarks_list:
+        if request.user == bookmark.user:
+            bookmarks.append(bookmark.project)
+
+    context = {
+        'projects' : projects_list,
+        'groups' : groups_list,
+        'universities' : universities_list,
+        'companies' : companies_list,
+        'bookmarks' : bookmarks
+    }
+    return render(request, 'index.html', context)
 
 def getTable(request):
-	return render(request, 'table.html')
+    return render(request, 'table.html')
 
 def getForm(request):
-	return render(request, 'form.html')
+    return render(request, 'form.html')
