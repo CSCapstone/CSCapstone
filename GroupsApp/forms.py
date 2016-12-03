085f9ee6d7b6c98f91a560da121f05205f57ca12
Grey <chen1474@purdue.edu>
2016-12-02 22:16:00 -0500

    finish up with groups edit

--- GroupsApp/forms.py
+++ GroupsApp/forms.py
@@ -3,7 +3,17 @@
 Created by Naman Patwari on 10/10/2016.
 """
 from django import forms
+from .models import Group
 
 class GroupForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
     description = forms.CharField(label='Description', max_length=300)
+
+class UpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Group
+        fields = ('name', 'description')
+        widgets = {
+            'description': forms.Textarea()
+        }
