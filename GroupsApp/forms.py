bffc80c4934724de45ca4295969bf3d4192481c6
Jacob Dunbar <dunbarj@purdue.edu>
2017-01-25 00:22:39 -0500

    Changed/Added to member-adding functionality. (#18)
    * Changed/Added to member-adding functionality.
    
    Changes:
    -When a user creates a new group they are now automatically added as
    the sole member.
    -Users can no longer add themselves to groups. They now can make a
    request to join a group which any current group member can view and
    approve/deny.
    -Users can still remove themselves from groups but cannot remove other
    members.
    -Users can add another user directly using their email address.
    
    * Added commenting system to Groups.
    
    -Added Comment model in the GroupsApp
    -Added the ability to add comments to groups
    -Comments include a timestamp
    -Only members of a group can post comments to it
    -Only the poster of a comment can delete it
    -Added Email column of group members list
    -Removed “Log In” and “Register” links on the home page

--- GroupsApp/forms.py
+++ GroupsApp/forms.py
@@ -7,3 +7,9 @@ from django import forms
 class GroupForm(forms.Form):
     name = forms.CharField(label='Name', max_length=30)
     description = forms.CharField(label='Description', max_length=300)
+	
+class GroupMemberForm(forms.Form):
+	email = forms.CharField(label='Email', max_length=50)
+	
+class CommentForm(forms.Form):
+	comment = forms.CharField(label='Comment', max_length=500)
--- GroupsApp/models.py
+++ GroupsApp/models.py
@@ -3,14 +3,22 @@
 Created by Naman Patwari on 10/10/2016.
 """
 from django.db import models
-from AuthenticationApp.models import Student
+from AuthenticationApp.models import MyUser
+from uuid import uuid4
 
 # Create your models here.
 class Group(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=300)
-    members = models.ManyToManyField(Student)
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=300)
+	#members = models.ManyToManyField(MyUser, related_name="members")
+	#requests = models.ManyToManyField(MyUser, related_name="requests")
     
-    
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
+	
+class Comment(models.Model):
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	poster = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+	text = models.CharField(max_length=500)
+	stamp = models.DateTimeField(auto_now_add=True)
+	cid = models.CharField(max_length=100, blank=True, unique=True, default=uuid4)
