87b9c3aea889b98d720cb1f5794ce11fd438810a
Jacob Dunbar <dunbarj@purdue.edu>
2017-02-06 12:46:07 -0500

    Adding GroupsApp/models.py back in.

---
+++ GroupsApp/models.py
@@ -0,0 +1,26 @@
+"""GroupsApp Models
+
+Created by Naman Patwari on 10/10/2016.
+"""
+from django.db import models
+from AuthenticationApp.models import MyUser
+from ProjectsApp.models import Project
+from uuid import uuid4
+
+# Create your models here.
+class Group(models.Model):
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=300)
+	members = models.ManyToManyField(MyUser, related_name="members")
+	requests = models.ManyToManyField(MyUser, related_name="requests")
+	project = models.OneToOneField(Project, on_delete=models.CASCADE, default="", primary_key=False, null=True)
+    
+	def __str__(self):
+		return self.name
+	
+class Comment(models.Model):
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	poster = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+	text = models.CharField(max_length=500)
+	stamp = models.DateTimeField(auto_now_add=True)
+	cid = models.CharField(max_length=100, blank=True, unique=True, default=uuid4)
