07457d3faf7be6381cdcb512d9e74257c8313687
Naman Patwari <thenamanpat@gmail.com>
2017-02-19 23:55:30 -0500

    This could be a potential bug

--- CSCapstoneApp/apps.py
+++ CSCapstoneApp/apps.py
@@ -3,7 +3,6 @@
 Created by Harris Christiansen on 9/18/16.
 """
 from django.apps import AppConfig
-from UniversitiesApp.models import University
 
 class CSCapstoneConfig(AppConfig):
     name = 'CSCapstone'
