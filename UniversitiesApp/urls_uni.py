6af0733afb1c785be7ce796237f4e5a695e28076
Naman Patwari <thenamanpat@gmail.com>
2017-02-20 10:34:24 -0500

    Basic search implemented.  (#23)
    * Universities are now accessed using id
    
    * Universities now use slugs
    
    * Trying to make prod work
    
    * Just added Watson support
    
    * Fixed merge issues
    
    * Fixed some merges.
    
    Please note course/ is not working.
    
    * This could be a potential bug
    
    * Fixed url matching - 2 url files

--- UniversitiesApp/urls_uni.py
+++ UniversitiesApp/urls_uni.py
@@ -15,11 +15,4 @@ urlpatterns = [
 
     url(r'^(?P<slug>[-\w\d]+)/join$', views.joinUniversity, name='university-join'),
     url(r'^(?P<slug>[-\w\d]+)/unjoin$', views.unjoinUniversity, name='university-unjoin'),
-
-	url(r'^course/form$', views.courseForm, name="CourseForm"),
-	url(r'^course/add$', views.addCourse, name="AddCourse"),
-	url(r'^course/remove$', views.removeCourse, name="RemoveCourse"),
-	url(r'^course/join$', views.joinCourse, name="JoinCourse"),
-	url(r'^course/unjoin$', views.unjoinCourse, name="UnjoinCourse"),
-	url(r'^course$', views.getCourse, name="Course"),    
 ]
